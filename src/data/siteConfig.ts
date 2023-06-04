2fa0aebbcb15b0b58ac8d7483cc6549d87348727
xxninjabunnyxx <xxninjabunnyxx@gmail.com>
2023-06-04 19:51:39 -0400

    changed data

---
+++ src/data/siteConfig.ts
@@ -0,0 +1,22 @@
+const siteConfig = {
+    name: 'SvelteKit',
+    socials: [
+        {
+            name: 'Twitter',
+            url: '',
+            icon: 'twitter',
+        },
+        {
+            name: 'GitHub',
+            url: '',
+            icon: 'github',
+        },
+        {
+            name: 'YouTube',
+            url: '',
+            icon: 'youtube',
+        },
+    ],
+};
+
+export default siteConfig;
