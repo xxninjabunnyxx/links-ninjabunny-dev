3f270aa2dcb9a54da84e4f8093f11a3394c38e5c
xxninjabunnyxx <xxninjabunnyxx@gmail.com>
2023-06-04 17:28:51 -0400

    moved into data folder

---
+++ src/data/socials.ts
@@ -0,0 +1,19 @@
+const socials: Socials = [
+    {
+        name: 'Twitter',
+        url: '',
+        icon: 'twitter',
+    },
+    {
+        name: 'GitHub',
+        url: '',
+        icon: 'github',
+    },
+    {
+        name: 'YouTube',
+        url: '',
+        icon: 'youtube',
+    },
+];
+
+export default socials;
